68a94869c5386bc37f92faf1a377cc2b6b68af8c
Tarique Anowar <anowartarique5@gmail.com>
2024-09-01 03:51:51 +0530

    FormSchema updated, all AI functionality added, new logic implemented.

--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -18,7 +18,7 @@ import CustomField from "./CustomField";
 import { aspectRatioOptions, creditFee, transformationTypes } from "@/constant";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { Button } from "../ui/button";
-import { useState, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
 import { Eraser, Save } from "lucide-react";
@@ -32,31 +32,59 @@ type TransformationFormProps = {
   config?: Transformations | null;
 };
 
-export const formSchema = z.object({
-  title: z.string(),
-  aspectRatio: z.string().optional(),
-  color: z.string().optional(),
-  prompt: z.string().optional(),
-  publicId: z.string(),
-});
+export const formSchema = z
+  .object({
+    title: z.string(),
+    aspectRatio: z.string().optional(),
+    type: z.string().min(1, "Type is required"), // Hidden field
+    color: z.string().optional(),
+    prompt: z.string().optional(),
+    publicId: z.string().min(1, "Choose Your Image").optional().nullable(),
+  })
+  .superRefine((data, context) => {
+    if (data.type === "fillBackground" && !data.aspectRatio) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["aspectRatio"],
+        message: "Choose Your Preferred Aspect Ratio",
+      });
+    }
+
+    if ((data.type === "remove" || data.type === "recolor") && !data.prompt) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["prompt"],
+        message: "Provide an Object Name",
+      });
+    }
+
+    if (data.type === "recolor" && !data.color) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["color"],
+        message: "Provide a Replacement Color",
+      });
+    }
+  });
 
 const TransformationForm = ({
   data = null,
   action,
   type,
   userId,
-  config,
+  config = null,
   creditBalance,
 }: TransformationFormProps) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isTransforming, setIsTransforming] = useState(false);
   const [newTransformaton, setNewTransformation] =
     useState<Transformations | null>(null);
-  const [image, setImage] = useState(data);
+  const [originalImage, setOriginalImage] = useState(data);
+  const [previewImage, setPreviewImage] = useState<IImage | null>(null);
   const [transformationConfig, setTransformationConfig] = useState(config);
   const [isPending, startTransition] = useTransition();
 
-  const transformationType = transformationTypes[type];
+  const transformationObject = transformationTypes[type];
 
   const initialValues =
     data && action === "Update"
@@ -66,6 +94,7 @@ const TransformationForm = ({
           color: data?.color,
           prompt: data?.prompt,
           publicId: data?.publicId,
+          type: transformationObject.type,
         }
       : {
           title: "",
@@ -73,6 +102,7 @@ const TransformationForm = ({
           color: "",
           prompt: "",
           publicId: "",
+          type: transformationObject.type,
         };
 
   // 1. Define your form.
@@ -83,23 +113,65 @@ const TransformationForm = ({
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    setIsSubmitting(true);
+
+    if (values.type === "fillBackground") {
+      const imageSize =
+        aspectRatioOptions[values.aspectRatio as AspectRatioKey];
+
+      setPreviewImage((prevState: any) => ({
+        ...prevState,
+        width: imageSize.width,
+        height: imageSize.height,
+        aspectRatio: values.aspectRatio,
+        publicId: values.publicId,
+      }));
+    } else {
+      setPreviewImage((prevState: any) => ({
+        ...prevState,
+        width: originalImage?.width,
+        height: originalImage?.height,
+        aspectRatio: values.aspectRatio,
+        publicId: values.publicId,
+      }));
+    }
+
+    /*
+    setTransformationConfig(transformationObject.config);
+
+    setTransformationConfig((prevState: any) => ({
+      ...prevState,
+      [type]: {
+        ["prompt"]: values.prompt,
+        ["to"]: values.color,
+      },
+    }));
+    */
+
+    setTransformationConfig((prevState: any) => ({
+      ...prevState,
+      [type]:
+        type === "fillBackground" ||
+        type === "removeBackground" ||
+        type === "restore"
+          ? true
+          : { prompt: values.prompt, to: values.color },
+    }));
+
+    console.log("TransformationForm Form values:", values);
+
+    console.log("TransformationForm previewImage:", previewImage);
+    return;
+
+    // startTransition(async () => {
+    //   await updateCredits(userId, creditFee);
+    // });
   }
 
-  // FIXME: Need to understand the code
   const onSelectFieldHandler = (
     value: string,
     onChangeField: (value: string) => void
   ) => {
-    const imageSize = aspectRatioOptions[value as AspectRatioKey];
-    setImage((prevState: any) => ({
-      ...prevState,
-      aspectRatio: imageSize.aspectRatio,
-      width: imageSize.width,
-      height: imageSize.height,
-    }));
-
-    setNewTransformation(transformationType.config);
     return onChangeField(value);
   };
 
@@ -123,48 +195,46 @@ const TransformationForm = ({
     return onChangeField(value);
   };
 
-  // TODO: Return to update credit fee
-  const onTransformationHandler = () => {
-    setIsTransforming(true);
-
-    setTransformationConfig(
-      deepMergeObjects(newTransformaton, transformationConfig)
+  useEffect(() => {
+    console.log(
+      "TransformationForm transformationConfig:",
+      transformationConfig
     );
+  }, [transformationConfig]);
 
-    setNewTransformation(null);
-
-    startTransition(async () => {
-      /*
-      await updateCredits(userId, creditFee);
-      */
-    });
-  };
+  const onTransformationHandler = () => {};
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* Hidden type field */}
+        <input type="hidden" {...form.register("type")} />
+
         <CustomField
           control={form.control}
           name="title"
-          formLabel="Image Title"
+          // formLabel="Image Title"
           className="w-full"
-          render={({ field }) => <Input {...field} className="input-field" />}
+          render={({ field }) => (
+            <Input {...field} placeholder="Image Title" className="w-full" />
+          )}
         />
 
-        {type === "fill" && (
+        {type === "fillBackground" && (
           <CustomField
             control={form.control}
             name="aspectRatio"
-            formLabel="Aspect Ratio"
+            // formLabel="Aspect Ratio"
             className="w-full"
             render={({ field }) => (
               <Select
+                value={form.watch("aspectRatio")}
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
               >
                 <SelectTrigger className="select-filed">
-                  <SelectValue placeholder="Select size" />
+                  <SelectValue placeholder="Preferred Image Size" />
                 </SelectTrigger>
                 <SelectContent>
                   {Object.keys(aspectRatioOptions).map((key) => (
@@ -182,12 +252,15 @@ const TransformationForm = ({
           <CustomField
             control={form.control}
             name="prompt"
-            formLabel={
-              type === "remove" ? "Object to Remove" : "Object to Recolor"
-            }
+            // formLabel={
+            //   type === "remove" ? "Object to Remove" : "Object to Recolor"
+            // }
             className="w-full"
             render={({ field }) => (
               <Input
+                placeholder={
+                  type === "remove" ? "Object to Remove" : "Object to Recolor"
+                }
                 value={field.value}
                 className="input-field"
                 onChange={(e) =>
@@ -207,10 +280,11 @@ const TransformationForm = ({
           <CustomField
             control={form.control}
             name="color"
-            formLabel="Replacement Color"
+            //formLabel="Replacement Color"
             className="w-full"
             render={({ field }) => (
               <Input
+                placeholder="Replacement Color"
                 value={field.value}
                 className="input-field"
                 onChange={(e) =>
@@ -233,35 +307,39 @@ const TransformationForm = ({
             className="flex size-full flex-col"
             render={({ field }) => (
               <MediaUploader
-                image={image}
+                originalImage={originalImage}
                 onValueChange={field.onChange}
-                setImage={setImage}
+                setOriginalImage={setOriginalImage}
                 publicId={field.value}
                 type={type}
               />
             )}
           />
 
-          <TransformedImage />
+          <TransformedImage
+            previewImage={previewImage}
+            setIsSubmitting={setIsSubmitting}
+            transformationConfig={transformationConfig}
+          />
         </div>
         <div className="flex flex-col gap-4">
-          <Button
+          {/* <Button
             type="button"
             className="submit-button capitalize"
             disabled={isTransforming || newTransformaton === null}
             onClick={onTransformationHandler}
           >
-            <Eraser className="w-4 h-4 mr-2" />
+            <Save className="w-4 h-4 mr-2" />
             {isTransforming ? "Transforming..." : "Apply Transformation"}
-          </Button>
+          </Button> */}
 
           <Button
             type="submit"
-            className="submit-button capitalize  bg-emerald-600 hover:bg-emerald-700 text-white"
+            className="capitalize  bg-emerald-600 hover:bg-emerald-700 text-white"
             disabled={isSubmitting}
           >
-            <Save className="w-4 h-4 mr-2" />
-            {isSubmitting ? "Saving..." : "Save Image"}
+            <Eraser className="w-4 h-4 mr-2" />
+            {isSubmitting ? "Transforming..." : "Apply Transformation"}
           </Button>
         </div>
       </form>
